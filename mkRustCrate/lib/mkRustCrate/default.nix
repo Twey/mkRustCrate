5533fa395c7730e26b83e7fec2d3a3236a4bcd73
James ‘Twey’ Kay <twey@twey.co.uk>
2018-11-13 01:06:48 +0000

    mkRustCrate: fix dependencies

--- mkRustCrate/lib/mkRustCrate/default.nix
+++ mkRustCrate/lib/mkRustCrate/default.nix
@@ -18,21 +18,21 @@ let
       chmod +x $out
     '';
   und = builtins.replaceStrings ["-"] ["_"];
-  wrapper = cmd: makeExecutable (substituteAll {
+  wrapper = cmd: substituteAll {
     src = ./wrapper.sh;
     inherit bash cmd;
-  });
+  };
   transDeps = k: builtins.concatMap (x: x.${k} ++ transDeps k x.${k});
   mkExterns = xs: builtins.concatStringsSep " " (map (x: if x == null then "" else
     "--extern ${und x.name}=${x}/lib/lib${und x.name}.rlib") xs);
   mkTrans = xs: builtins.concatStringsSep " " (map (x: "-L dependency=${x}/lib") xs);
-  depFlags = xs: k: let v = xs.${k}; in mkExterns v + " " + mkTrans (transDeps k v)
+  depFlags = k: v: mkExterns v + " " + mkTrans (transDeps k v);
 in
 stdenv.mkDerivation ({
   inherit buildProfile dependencies devDependencies buildDependencies;
-  dependenciesFlags = depFlags "dependencies" args';
-  devDependenciesFlags = depFlags "devDependencies" args';
-  buildDependenciesFlags = depFlags "buildDependencies" args';
+  dependenciesFlags = depFlags "dependencies" dependencies;
+  devDependenciesFlags = depFlags "devDependencies" devDependencies;
+  buildDependenciesFlags = depFlags "buildDependencies" buildDependencies;
   cargo = "${cargo}/bin/cargo";
   jq = "${jq}/bin/jq";
   remarshal = "${remarshal}/bin/remarshal";
@@ -47,6 +47,6 @@ stdenv.mkDerivation ({
   features = builtins.concatStringsSep " " features;
   buildPhase = ". ${./build.sh}";
   installPhase = ". ${./install.sh}";
-  RUSTC = wrapper "${rust}/bin/rustc";
-  RUSTDOC = wrapper "${rust}/bin/rustdoc";
+  RUSTC = wrapper "${rust.rustc}/bin/rustc";
+  RUSTDOC = wrapper "${rust.rustc}/bin/rustdoc";
 } // args')
